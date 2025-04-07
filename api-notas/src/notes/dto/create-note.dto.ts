c052c07d34b4c8971f804fc79ae0f13dfeaa1e21
Cupid0nn <malaucierno@gmail.com>
2025-04-06 21:39:02 -0300

    dto´s

--- api-notas/src/notes/dto/create-note.dto.ts
+++ api-notas/src/notes/dto/create-note.dto.ts
@@ -1,1 +1,13 @@
-export class CreateNoteDto {}
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateNoteDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isFavorite?: boolean;
+}
