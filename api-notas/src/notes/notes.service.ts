655a05dc9181067aa1979d430488b3bede4b679c
Cupid0nn <malaucierno@gmail.com>
2025-04-07 12:58:52 -0300

    testv1

--- api-notas/src/notes/notes.service.ts
+++ api-notas/src/notes/notes.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Note } from './entities/note.entity';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Injectable()
 export class NotesService {
-  create(createNoteDto: CreateNoteDto) {
-    return 'This action adds a new note';
+  constructor(
+    @InjectRepository(Note)
+    private notesRepository: Repository<Note>,
+  ) {}
+
+  async create(createNoteDto: CreateNoteDto): Promise<Note> {
+    const note = this.notesRepository.create(createNoteDto);
+    return await this.notesRepository.save(note);
   }
 
-  findAll() {
-    return `This action returns all notes`;
+  async findAll(): Promise<Note[]> {
+    return await this.notesRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} note`;
+  async findOne(id: string): Promise<Note> {
+    const note = await this.notesRepository.findOne({ where: { id } });
+    if (!note) throw new NotFoundException('Nota no encontrada');
+    return note;
   }
 
-  update(id: number, updateNoteDto: UpdateNoteDto) {
-    return `This action updates a #${id} note`;
+  async update(id: string, updateNoteDto: UpdateNoteDto): Promise<Note> {
+    const note = await this.notesRepository.preload({
+      id,
+      ...updateNoteDto,
+    });
+    if (!note) throw new NotFoundException('Nota no encontrada');
+    return this.notesRepository.save(note);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} note`;
+  async remove(id: string): Promise<void> {
+    const note = await this.findOne(id);
+    await this.notesRepository.remove(note);
   }
+
+  async findFavorites(): Promise<Note[]> {
+    return await this.notesRepository.find({
+      where: { isFavorite: true },
+    });
+  }
+  
 }
+
+
